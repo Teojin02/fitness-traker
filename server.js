0ccf1de4a1d523e2673773d96338d463c411fd33
Your Name <you@example.com>
2020-10-15 23:50:07 -0700

    added apiRoutes.js

--- server.js
+++ server.js
@@ -2,14 +2,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
